4205ea6469f8bebcd620e1c75ae64f0b2e2882eb
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-09-04 02:55:58 +0530

    disabled 404 dev options

--- holdings/js/404.js
+++ holdings/js/404.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
 
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
+   $('#menu').click(function(){
+       $(this).toggleClass('fa-times');
+       $('.navbar').toggleClass('nav-toggle');
+   });
+
+   $(window).on('scroll load',function(){
+       $('#menu').removeClass('fa-times');
+       $('.navbar').removeClass('nav-toggle');
+
+       if(window.scrollY>60){
+           document.querySelector('#scroll-top').classList.add('active');
+       }else{
+           document.querySelector('#scroll-top').classList.remove('active');
+       }
+
+       // scroll spy
+       $('section').each(function(){
+           let height = $(this).height();
+           let offset = $(this).offset().top - 200;
+           let top = $(window).scrollTop();
+           let id = $(this).attr('id');
+
+           if(top>offset && top<offset+height){
+               $('.navbar ul li a').removeClass('active');
+               $('.navbar').find(`[href="#${id}"]`).addClass('active');
+           }
+       });
+   });
+
+   // smooth scrolling
+   $('a[href*="#"]').on('click',function(e){
+       e.preventDefault();
+       $('html, body').animate({
+           scrollTop : $($(this).attr('href')).offset().top,
+       },500, 'linear')
+   })
 });
 
 
@@ -27,16 +59,16 @@ document.onkeydown = function(e) {
   if(e.keyCode == 123) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'c'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'v'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'a'.charCodeAt(0)) {
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'V'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.keyCode == 'i'.charCodeAt(0)) {
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
      return false;
   }
 }
